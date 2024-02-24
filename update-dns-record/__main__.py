30fa2c53cc68218b8e592cfad200bdc010eedb65
Steven Gillies <stevengillies@outlook.com>
2024-02-24 00:36:22 +0000

    detected host ip

--- update-dns-record/__main__.py
+++ update-dns-record/__main__.py
@@ -3,9 +3,13 @@ import logging.handlers
 from typing import Literal, Optional
 
 import click
+import requests
 
 logging.basicConfig(
     level=logging.INFO,
+    format="{asctime} {levelname} {msg}",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    style="{",
     handlers=[
         logging.StreamHandler(),
         logging.handlers.RotatingFileHandler(
@@ -36,6 +40,18 @@ def main(provider: Literal["cloudflare"], record_name: str, ip: Optional[str] = 
     RECORD_NAME: The name of the record in the provider's system.
     """
     logging.info(f"Updating DNS record '{record_name}' with provider '{provider}'")
+    ip = ip or get_public_ip()
+    logging.info(f"Setting '{record_name}' record to IP '{ip}'")
+
+
+def get_public_ip() -> str:
+    r = requests.get("https://api.ipify.org", params={"format": "json"})
+    try:
+        r.raise_for_status()
+    except Exception as exc:
+        logging.error(exc)
+        raise
+    return r.json()["ip"]
 
 
 if __name__ == "__main__":
